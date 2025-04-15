f82358e285d590392b3a8db1dfa003e28fc0522a
darshita-kumar <36875506+darshita-kumar@users.noreply.github.com>
2025-04-15 09:11:43 +0530

    Update DisjointSetUnion.java

--- Notes DSA/DisjointSetUnion.java
+++ Notes DSA/DisjointSetUnion.java
@@ -5,64 +5,53 @@ import java.util.*;
 import static java.lang.System.out;
 
 class GFG {
-
-    static int parent[];
-    static int ranks[];
-
-    static void initialize() {
-        for (int i = 0; i < parent.length; i++) {
-            parent[i] = i;
-        }
-    }
-
-    static int find(int x) {
-        if (parent[x] == x)
-            return x;
-
-        parent[x] = find(parent[x]);
-
-        return parent[x];
-
-    }
-
-    static void union(int x, int y) {
-        int x_rep = find(x);
-        int y_rep = find(y);
-
-        if (x_rep == y_rep)
-            return;
-
-        // jiski rank kam h uska parent update hoga
-        if (ranks[x_rep] < ranks[y_rep])
-            parent[x_rep] = y_rep;
-
-        else if (ranks[y_rep] < ranks[x_rep])
-            parent[y_rep] = x_rep;
-
-        else {
-            // same rank h to ek ka parent update krdo, dusre ki rank update krdo
-            parent[y_rep] = x_rep;
-
-            ranks[x_rep]++;
-        }
-
-    }
-
     public static void main(String[] args) {
 
         int n = 5;
-
-        parent = new int[5];
-        ranks = new int[5];
-
-        initialize();
-
-        union(3, 4);
-        union(2, 3);
-        union(1, 2);
-        union(0, 1);
-
-        out.println(parent[3]);
-        out.println(ranks[3]);
+        DisjointSet ds = new DisjointSet(n);
+        ds.unionBySize(3, 4);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(1, 2);
+        ds.unionBySize(0, 1);
+        ds.unionBySize(5, 6);
+        
+        out.println("Do 1 and 3 belong to same component?: "+(ds.findUParent(1)==ds.findUParent(3)));
+        out.println("Do 1 and 5 belong to same component?: "+(ds.findUParent(1)==ds.findUParent(5)));
     }
 }
+
+class DisjointSet {
+    int[] parent;
+    int[] size;
+    public DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for(int i=0; i<n; i++) {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    public int findUParent(int node) {
+        if(node == parent[node])
+            return node;
+        // Path compression
+        parent[node] = findUParent(node);
+        return parent[node];
+    }
+
+    public void unionBySize(int u, int v) {
+        int parentU = findUParent(u);
+        int parentV = findUParent(v);
+        if(parentU == parentV) return;
+        // Attach smaller to larger
+        if(size[parentU] > size[parentV]) {
+            parent[parentV] = parentU;
+            size[parentU] += size[parentV];
+        } else {
+            parent[parentU] = parentV;
+            size[parentV] += size[parentU];
+        }
+    }
+}
+
